0ae270063b8af46d89a7cd4d83ffff6c600494fc
Iscle <albertiscle9@gmail.com>
2025-06-01 20:19:50 +0200

    chore: remove graphql

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,29 +1,17 @@
 import env from '@/config/env';
 import { di } from '@/server/di';
-import getGQL from '@/server/plugins/gql';
 import AutoLoad from '@fastify/autoload';
 import Cors from '@fastify/cors';
 import Helmet from '@fastify/helmet';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import UnderPressure from '@fastify/under-pressure';
 import { FastifyInstance } from 'fastify';
-import mercurius from 'mercurius';
 import path from 'node:path';
 
 export default async function createServer(fastify: FastifyInstance) {
-  // Graphql
-  await fastify.register(mercurius, {
-    schema: await getGQL(),
-    graphiql: env.isDevelopment,
-    defineMutation: true,
-  });
-
   // Set sensible default security headers
   await fastify.register(Helmet, {
     global: true,
-    // The following settings are needed for graphiql, see https://github.com/graphql/graphql-playground/issues/1283
-    contentSecurityPolicy: !env.isDevelopment,
-    crossOriginEmbedderPolicy: !env.isDevelopment,
   });
 
   // Enables the use of CORS in a Fastify application.
